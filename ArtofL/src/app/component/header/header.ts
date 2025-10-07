8afd54f5b4d84de61eddc07c120130f926033478
Jhon <doedevjhon@gmail.com>
2025-10-06 21:34:26 -0500

    slight changes

--- ArtofL/src/app/component/header/header.ts
+++ ArtofL/src/app/component/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../nav-bar/nav-bar';
+import {PostCreate} from '../post-create/post-create';
 
 @Component({
   selector: 'app-header',
-  imports: [NavBar],
+  imports: [NavBar, PostCreate],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
--- ArtofL/src/app/component/image-upload/image-upload.ts
+++ ArtofL/src/app/component/image-upload/image-upload.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-image-upload',
   imports: [],
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './image-upload.css'
 })
 export class ImageUpload {
+
 
 }
