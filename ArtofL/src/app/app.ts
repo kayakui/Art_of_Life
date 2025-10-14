379861d3742699ae8c34f6d827d375f79e7199d5
Jhon <doedevjhon@gmail.com>
2025-10-13 20:17:33 -0500

    footer prototype

--- ArtofL/src/app/app.ts
+++ ArtofL/src/app/app.ts
@@ -3,10 +3,11 @@ import { Home } from './home/home';
 import { Header } from './component/header/header';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import {Footer} from './component/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, Home, Header, RouterLink, RouterLinkActive, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
