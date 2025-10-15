3f2ffccf7d7365fd1ff654d4bf9f9cc6778ee6e1
Jhon <doedevjhon@gmail.com>
2025-10-14 23:27:20 -0500

    page fix + about page + welcome banner on home

--- ArtofL/src/app/app.ts
+++ ArtofL/src/app/app.ts
@@ -1,15 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { Home } from './pages/home/home';
 import { Header } from './component/header/header';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import {Footer} from './component/footer/footer';
-import {LeftSideBar} from './component/side_bars/left-side-bar/left-side-bar';
-import {RightSideBar} from './component/side_bars/right-side-bar/right-side-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header, RouterLink, RouterLinkActive, Footer, LeftSideBar, RightSideBar],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
