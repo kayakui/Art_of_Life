f5686131952c71dec7dfe938930dec06cb226b15
Jhon <doedevjhon@gmail.com>
2025-10-22 23:07:12 -0500

    prototype of image display, change and improve

--- ArtofL/src/app/component/visual-arts-page/visual-arts-page.ts
+++ ArtofL/src/app/component/visual-arts-page/visual-arts-page.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './visual-arts-page.css'
 })
 export class VisualArtsPage {
+  constructor() {
+  }
 
+  ngOnInit() {
+    const block = document.createElement('div');
+    const block2 = document.createElement('div');
+    const img = document.createElement('img');
+    const img2 = document.createElement('img');
+    img.setAttribute('src', "images/header_column.png");
+    img2.setAttribute('src', "images/header_column.png");
+    let img_height = img.height;
+    let img_width = img.width;
+    block.style.cssText = `background-color: #fff; display: flex; width: ${img_width + 100}px; height: ${img_height + 100}px;`;
+    block2.style.cssText = `background-color: #fff; display: flex; width: ${img_width + 100}px; height: ${img_height + 100}px;`;
+    img.style.cssText = `width: ${img_width}px; height: ${img_height}px; margin: auto;`;
+    img2.style.cssText = `width: ${img_width}px; height: ${img_height}px; margin: auto;`;
+    block.appendChild(img);
+    block2.appendChild(img2);
+    document.getElementById('images_container')?.appendChild(block);
+    document.getElementById('images_container')?.appendChild(block2);
+  }
 }
